406d80691359d35030dacb82c9ab031b39556443
Akito Tajitsu <tajitsu@naoj.org>
2019-06-14 00:24:12 -1000

    macOS ready

--- makeosx.sh
+++ makeosx.sh
@@ -2,5 +2,8 @@
 touch configure configure.ac aclocal.m4 Makefile.am Makefile.in
 
 ## libffi & openssl installed as keg-only in Homebrew
-./configure --with-gtkmac PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig:/usr/local/opt/openssl/lib/pkgconfig" \
+./configure --with-gtkmac \
+	    --with-localedir=Contents/Resources/share/locale \
+	    --with-themedir=Contents/Resources/share/icons \
+	    PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig:/usr/local/opt/openssl/lib/pkgconfig" \
   && make
