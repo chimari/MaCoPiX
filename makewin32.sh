d543af434718c12fc495773a933b54694516c9ca
Akito Tajitsu <tajitsu@naoj.org>
2019-06-17 10:22:52 -1000

    macOS ready

--- makewin32.sh
+++ makewin32.sh
@@ -4,4 +4,4 @@ touch configure configure.ac aclocal.m4 Makefile.am Makefile.in
 ./configure --with-win32 --with-tar32\
   'CC=gcc' CFLAGS=-O2 \
   && make \
-  && make install-strip prefix=/home/taji/dist32
+  && make install-strip prefix=/home/${USER}/dist32
--- makewin64.sh
+++ makewin64.sh
@@ -4,4 +4,4 @@ touch configure configure.ac aclocal.m4 Makefile.am Makefile.in
 ./configure --with-win32 --with-tar64\
   'CC=gcc' CFLAGS=-O2 \
   && make \
-  && make install-strip prefix=/home/taji/dist64
+  && make install-strip prefix=/home/${USER}/dist64
